c740361c8167e52ab15bc04f26b5c34f17a10883
Erica Little <eteney90@gmail.com>
2019-11-17 17:25:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
 if array.size == 1
   puts array
 elsif array.size == 2
-  array.split("and")
+  array.join("and")
 elsif array.size > 2
-  array.split(",").insert(2, "and")
+  array.join(",").insert(2, "and")
 end
 end
